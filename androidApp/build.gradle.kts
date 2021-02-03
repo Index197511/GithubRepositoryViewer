df399d0011c90f8579e5d214abd7786237865a91
Index197511 <hiro.saten@gmail.com>
2021-02-03 13:54:17 +0900

    set up JetpackCompose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,6 +8,12 @@ dependencies {
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+
+    // Jetpack Compose
+    val composeVersion = "1.0.0-alpha10"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
 
 android {
@@ -23,5 +29,20 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-alpha10"
+        kotlinCompilerVersion = "1.4.21"
     }
 }
--- androidApp/src/main/java/me/index197511/githubrepositoryviewer/androidApp/MainActivity.kt
+++ androidApp/src/main/java/me/index197511/githubrepositoryviewer/androidApp/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.index197511.githubrepositoryviewer.shared.Greeting
 import android.widget.TextView
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 
 fun greet(): String {
     return Greeting().greeting()
@@ -14,7 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+//        val tv: TextView = findViewById(R.id.text_view)
+//        tv.text = greet()
+        setContent {
+            Hello()
+        }
     }
+}
+
+@Composable
+fun Hello() {
+    Text(text = "Hello")
+}
+
+@Preview
+@Composable
+fun HelloPreview() {
+    Hello()
 }
