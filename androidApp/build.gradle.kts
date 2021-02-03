81a0dd921491911a7d20e2c3c5ca62dd4251c76d
Index197511 <hiro.saten@gmail.com>
2021-02-03 14:01:27 +0900

    add Navigation and Androidx dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,6 +9,21 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.2")
 
+    // Androidx
+    val androidxCoreVersion = "1.3.2"
+    val androidxAppCompatVersion = "1.2.0"
+    val androidxLifeCycleVersion = "2.3.0-rc01"
+    val androidxFragmentVersion = "1.3.0-rc01"
+    implementation("androidx.core:core-ktx:$androidxCoreVersion")
+    implementation("androidx.appcompat:appcompat:$androidxAppCompatVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$androidxLifeCycleVersion")
+    implementation("androidx.fragment:fragment-ktx:$androidxFragmentVersion")
+
+    // Navigation
+    val navigationVersion = "2.3.2"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
     // Jetpack Compose
     val composeVersion = "1.0.0-alpha10"
     implementation("androidx.compose.ui:ui:$composeVersion")
