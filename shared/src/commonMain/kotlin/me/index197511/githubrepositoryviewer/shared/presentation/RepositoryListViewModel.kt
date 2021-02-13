111a241ca7bf3bc82e654b6c3e43680ec10b907f
Index197511 <hiro.saten@gmail.com>
2021-02-13 10:35:40 +0900

    replace TrendRepositoryViewModel with RepositoryListViewModel

---
+++ shared/src/commonMain/kotlin/me/index197511/githubrepositoryviewer/shared/presentation/RepositoryListViewModel.kt
@@ -0,0 +1,34 @@
+package me.index197511.githubrepositoryviewer.shared.presentation
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import me.index197511.githubrepositoryviewer.shared.data.repository.GithubRepository
+import me.index197511.githubrepositoryviewer.shared.data.resource.remote.GithubService
+import me.index197511.githubrepositoryviewer.shared.model.DataState
+import me.index197511.githubrepositoryviewer.shared.model.Repository
+import me.index197511.githubrepositoryviewer.shared.model.repository.IGithubRepository
+
+class RepositoryListViewModel : ViewModel() {
+    // TODO: DI
+    private val repository: IGithubRepository = GithubRepository(GithubService())
+
+    @ExperimentalCoroutinesApi
+    private val _repositories: MutableStateFlow<DataState<List<Repository>>> =
+        MutableStateFlow(DataState.Empty)
+
+    @ExperimentalCoroutinesApi
+    val repositories: StateFlow<DataState<List<Repository>>>
+        get() = _repositories
+
+    @ExperimentalCoroutinesApi
+    fun getRepositories() {
+        viewModelScope.launch {
+            repository.getTrendRepositories().collect {
+                _repositories.value = it
+            }
+        }
+    }
+}
